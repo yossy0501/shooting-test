060ff2d1e93a4ed0b0a1ea4e1e1daddaee909321
yossy0501 <yoshimoto.takuya.develop@gmail.com>
2018-12-11 20:38:50 +0900

    added project

---
+++ shooting/Assets/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    int score = 0;
+    GameObject scoreText;
+    GameObject gameOverText;
+
+    public void GameOver()
+    {
+        this.gameOverText.GetComponent<UnityEngine.UI.Text>().text = "GameOver";
+    }
+
+    public void AddScore()
+    {
+        this.score += 10;
+    }
+
+    void Start()
+    {
+        this.scoreText = GameObject.Find("Score");
+        this.gameOverText = GameObject.Find("GameOver");
+    }
+
+    void Update()
+    {
+        scoreText.GetComponent<UnityEngine.UI.Text>().text = "Score:" + score.ToString("D4");
+    }
+}
